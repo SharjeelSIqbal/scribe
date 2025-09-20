f2b37a6ff642553d94afd154f1529b6361af8e51
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2025-09-20 10:18:35 -0700

    #2 added user roles to context

--- src/components/notes/Notes.tsx
+++ src/components/notes/Notes.tsx
@@ -15,14 +15,17 @@ function Notes(): JSX.Element {
     <NoteContainer>
       <div className="flex w-full flex-col lg:flex-col">
         <div className="py-1.5 relative h-14">
-          <input
-            value={title}
-            onChange={handleChangeTitle}
-            type="text"
-            placeholder={PLACEHOLDER_TEXT_TITLE}
-            className="absolute title-input input-ghost input-2xl w-full focus:outline-none bg-transparent focus:bg-transparent text-transparent carat-primary placeholder:text-secondary-content"
-          />
-          <h1 className="note-title">{title}</h1>
+          <div>
+            <input
+              value={title}
+              onChange={handleChangeTitle}
+              type="text"
+              placeholder={PLACEHOLDER_TEXT_TITLE}
+              className="absolute title-input input-ghost input-2xl w-full focus:outline-none bg-transparent focus:bg-transparent text-transparent carat-primary placeholder:text-secondary-content"
+            />
+            <h1 className="note-title">{title}</h1>
+          </div>
+          <div />
         </div>
         <div className="py-1.5">
           <Editor />
