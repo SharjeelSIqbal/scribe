cb8b8538ca11457add978d412a123ec7cd9e1e19
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2025-09-28 14:06:01 -0700

    #2 Added fonts and fixed issues with title input and header

--- src/components/notes/Notes.tsx
+++ src/components/notes/Notes.tsx
@@ -3,7 +3,7 @@ import NoteContainer from './NoteContainer';
 import Editor from '../rich-text-editor/Editor';
 import UserRoleDropdown from '../rich-text-editor/UserRoleDropdown';
 import { useUserRole } from '../../contexts/UserRoleContext';
-import { USER_ROLE_EDITOR } from '../../libs/constants';
+import { USER_ROLE_EDITOR, USER_ROLE_VIEWER } from '../../libs/constants';
 
 const PLACEHOLDER_TEXT_TITLE = 'Title';
 
@@ -19,16 +19,20 @@ function Notes(): JSX.Element {
     <NoteContainer>
       <div className="flex w-full flex-col lg:flex-col">
         <div className="py-1.5 relative h-14 flex justify-between items-center">
-          <div className="w-[90%] relative">
-            <input
-              value={title}
-              onChange={handleChangeTitle}
-              type="text"
-              placeholder={userRole === USER_ROLE_EDITOR ? PLACEHOLDER_TEXT_TITLE : ''}
-              className="title-input title-no-margin absolute input-ghost input-2xl w-full focus:outline-none bg-transparent focus:bg-transparent text-transparent carat-primary placeholder:text-secondary-content mb-0 bottom-0"
-              disabled={userRole !== USER_ROLE_EDITOR}
-            />
-            <h1 className="note-title title-no-margin mb-0">{title}</h1>
+          <div className="w-[90%] relative min-h-4 h-full flex items-center">
+            {userRole === USER_ROLE_EDITOR && (
+              <input
+                value={title}
+                onChange={handleChangeTitle}
+                type="text"
+                placeholder={userRole === USER_ROLE_EDITOR ? PLACEHOLDER_TEXT_TITLE : ''}
+                className="focus-within:bg-transparent title-no-margin input-ghost input-2xl w-full focus:outline-none bg-transparent focus:bg-transparent carat-primary placeholder:text-secondary-content mb-0 "
+                disabled={userRole !== USER_ROLE_EDITOR}
+              />
+            )}
+            {userRole === USER_ROLE_VIEWER && (
+              <h1 className="note-title title-no-margin mb-0">{title}</h1>
+            )}
           </div>
           <UserRoleDropdown />
         </div>
